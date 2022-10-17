548768cbf466a11851b2a0239df67a5fa7a5062f
kensonjohnson <kenson.johnson@outlook.com>
2022-10-16 21:05:54 -0500

    Can Now Check Against Three of a Kind
    evaluate.js can now sort out if a hand contains a Full House or a
    Three of a Kind.

--- evaluate.js
+++ evaluate.js
@@ -49,6 +49,54 @@ const testHand4 = [
   new Card("S", "Q"),
 ];
 
+const testHand5 = [
+  new Card("S", "3"),
+  new Card("C", "3"),
+  new Card("D", "3"),
+  new Card("H", "3"),
+  new Card("S", "Q"),
+];
+
+const testHand6 = [
+  new Card("S", "2"),
+  new Card("C", "2"),
+  new Card("D", "3"),
+  new Card("H", "3"),
+  new Card("S", "3"),
+  new Card("C", "8"),
+  new Card("H", "8"),
+];
+
+const testHand7 = [
+  new Card("S", "2"),
+  new Card("C", "2"),
+  new Card("D", "3"),
+  new Card("H", "3"),
+  new Card("S", "3"),
+  new Card("C", "9"),
+  new Card("H", "A"),
+];
+
+const testHand8 = [
+  new Card("S", "4"),
+  new Card("C", "4"),
+  new Card("D", "4"),
+  new Card("H", "8"),
+  new Card("S", "8"),
+  new Card("C", "8"),
+  new Card("H", "A"),
+];
+
+const testHand9 = [
+  new Card("S", "2"),
+  new Card("C", "4"),
+  new Card("D", "4"),
+  new Card("H", "4"),
+  new Card("S", "7"),
+  new Card("C", "8"),
+  new Card("H", "A"),
+];
+
 let dupCount = 1;
 let seqCount = 1;
 let seqCountMax = 1;
@@ -71,11 +119,30 @@ export default function getPlayerHandRank(playerCards, tableCards) {
   let sorted = playerCards.concat(tableCards).sort((a, b) => {
     return a.numericValue - b.numericValue;
   });
+
+  //This is for testing specific hands
+  //   sorted = testHand9;
+
   //prepare all of the data we need to determine the hand score
   analyzeHand(sorted);
-  if (checkFlush(testHand4)) {
+
+  // Check for a Royal Flush, Straight Flush, or Flush
+  if (checkFlush(sorted)) {
     return [rankName, rank, rankCards];
   }
+  console.log(duplicates);
+  // Check for a Four of a Kind
+  if (checkFourOfAKind(sorted)) {
+    return { rankName, rank, rankCards };
+  }
+
+  // Check for a Three of a Kind or Full House
+
+  if (checkThreeOfAKind(sorted)) {
+    return { rankName, rank, rankCards };
+  }
+
+  return "Not a Winner";
 }
 
 //Many hands leave room for extra cards to be included in the mix. Such as having a Pair:
@@ -85,6 +152,7 @@ function evaluateRankByHighestCards(
   arrayOfCards,
   excludedCard1 = -1,
   excludedCard2 = -1,
+  limitCheck = 7, // The most cards we can expect are 7
   normalize = 433175 // this brings the final sum back down to a number between 0 and 1
 ) {
   let sum = 0;
@@ -96,11 +164,16 @@ function evaluateRankByHighestCards(
     }
     let normalizedValue = cardValue - 2;
     sum += normalizedValue * Math.pow(13, i);
+    if (i >= limitCheck - 1) {
+      break;
+    }
   }
 
   return sum / normalize;
 }
 
+// Before we get into determining if a hand is worth anything, we should identify cards that match by face value or by suit.
+// We also grab some other metrics within this function.
 function analyzeHand(sortedCards) {
   //reset the variables
   dupCount = 1;
@@ -115,10 +188,12 @@ function analyzeHand(sortedCards) {
   nextCardSuit = null;
   duplicates = [];
   rankName = "";
+  rank = 0;
+  rankCards = [];
 
   //grab the highest value represented
   maxCardValue = sortedCards[sortedCards.length - 1].numericValue;
-  for (let i = 0; i < 6; i++) {
+  for (let i = 0; i < sortedCards.length - 1; i++) {
     currentCardValue = sortedCards[i].numericValue;
     currentCardSuit = sortedCards[i].suit;
 
@@ -149,9 +224,16 @@ function analyzeHand(sortedCards) {
     seqMaxValue = nextCardValue;
   }
 
+  // If we find any matching cards, we want to save how many and what the card value is.
   if (dupCount > 1) {
-    duplicates.push({ dupCount, nextCardValue });
+    currentCardValue = nextCardValue;
+    duplicates.push({ dupCount, currentCardValue });
   }
+
+  // Finally, to make the matches easier to work with, we will move the highest number of matches to the front of the array
+  duplicates.sort((a, b) => {
+    return b.dupCount - a.dupCount;
+  });
 }
 
 function checkFlush(sortedCards) {
@@ -228,4 +310,155 @@ function checkRoyalFlush(suitedCards) {
     return true;
   }
   return false;
+}
+
+// Only checks for a Four of a kind, and the score it returns is simply the those four cards plus the highest other card available.
+function checkFourOfAKind(sortedCards) {
+  if (duplicates.length > 0 && duplicates[0].dupCount === 4) {
+    rankName = "Four of a Kind";
+    rank =
+      700 +
+      (duplicates[0].currentCardValue / 14) * 50 +
+      evaluateRankByHighestCards(
+        sortedCards,
+        duplicates[0].currentCardValue, // leave out the cards included in the Four of a Kind
+        -1,
+        1 // We only want the single highest card other than the Four of a Kind to be included.
+      );
+    rankCards = sortedCards.slice(-5);
+    return true;
+  }
+  return false;
+}
+
+// This function will check for a Three of a Kind, and also do another check for a Full House.
+// A Full House is a three of a Kind and a Pair. If there are two Pairs available, it picks the highest valued Pair.
+// If only a Three of a Kind is found, the score includes those cards plus the two highest valued cards left.
+function checkThreeOfAKind(sortedCards) {
+  if (duplicates.length > 0 && duplicates[0].dupCount === 3) {
+    // check if a Full House is present
+    // First Edge Case: if there are two Pair available, take the highest pair
+    if (
+      duplicates.length > 2 &&
+      duplicates[0].dupCount === 3 &&
+      duplicates[1].dupCount === 2 &&
+      duplicates[2].dupCount === 2
+    ) {
+      rankName = "Full House";
+      let highestPairValue = Math.max(
+        duplicates[1].currentCardValue,
+        duplicates[2].currentCardValue
+      );
+      rank = 600 + duplicates[0].currentCardValue + highestPairValue / 14;
+      let tempThree = sortedCards.filter((card) => {
+        if (card.numericValue === duplicates[0].currentCardValue) {
+          return true;
+        }
+      });
+      let tempTwo = sortedCards.filter((card) => {
+        if (card.numericValue === highestPairValue) {
+          return true;
+        }
+      });
+
+      rankCards = tempThree.concat(tempTwo);
+    }
+    //Look at a normal Full House
+    else if (
+      duplicates.length > 1 &&
+      duplicates[0].dupCount === 3 &&
+      duplicates[1].dupCount === 2
+    ) {
+      rankName = "Full House";
+      rank =
+        600 +
+        duplicates[0].currentCardValue +
+        duplicates[1].currentCardValue / 14;
+      let tempThree = sortedCards.filter((card) => {
+        if (card.numericValue === duplicates[0].currentCardValue) {
+          return true;
+        }
+      });
+      let tempTwo = sortedCards.filter((card) => {
+        if (card.numericValue === duplicates[1].currentCardValue) {
+          return true;
+        }
+      });
+
+      rankCards = tempThree.concat(tempTwo);
+    }
+    // Second Edge Case: Two sets of Three of a Kind
+    else if (
+      duplicates.length > 1 &&
+      duplicates[0].dupCount === 3 &&
+      duplicates[1].dupCount === 3
+    ) {
+      rankName = "Full House";
+      let rank1 =
+        600 +
+        duplicates[0].currentCardValue +
+        duplicates[1].currentCardValue / 14;
+      let rank2 =
+        600 +
+        duplicates[1].currentCardValue +
+        duplicates[0].currentCardValue / 14;
+
+      if (rank1 > rank2) {
+        rank = rank1;
+        let tempThree = sortedCards.filter((card) => {
+          if (card.numericValue === duplicates[0].currentCardValue) {
+            return true;
+          }
+        });
+        let tempTwo = sortedCards.filter((card) => {
+          if (card.numericValue === duplicates[1].currentCardValue) {
+            return true;
+          }
+        });
+
+        rankCards = tempThree.concat(tempTwo.slice(-2));
+      } else {
+        rank = rank2;
+        let tempThree = sortedCards.filter((card) => {
+          if (card.numericValue === duplicates[1].currentCardValue) {
+            return true;
+          }
+        });
+        let tempTwo = sortedCards.filter((card) => {
+          if (card.numericValue === duplicates[0].currentCardValue) {
+            return true;
+          }
+        });
+
+        rankCards = tempThree.concat(tempTwo.slice(-2));
+      }
+    } else if (duplicates.length === 1 && duplicates[0].dupCount === 3) {
+      rankName = "Three of a Kind";
+      rank =
+        300 +
+        (duplicates[0].currentCardValue / 14) *
+          (50 +
+            evaluateRankByHighestCards(
+              sortedCards,
+              duplicates[0].currentCardValue,
+              -1,
+              2 // We only want to two highest cards other than the triple
+            ));
+
+      let tempThree = sortedCards.filter((card) => {
+        if (card.numericValue === duplicates[0].currentCardValue) {
+          return true;
+        }
+      });
+      let tempTwo = sortedCards.filter((card) => {
+        if (card.numericValue != duplicates[0].currentCardValue) {
+          return true;
+        }
+      });
+
+      rankCards = tempThree.concat(tempTwo.slice(-2));
+    }
+
+    return true;
+  }
 }
